58bea1fd29c22e16f2cbdaccc26832e6fe87ee5a
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-05 09:57:10 +0100

    Add day 5 part 1 & 2

---
+++ src/2022/Day05/Solver.cs
@@ -0,0 +1,163 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace _2022.Day05;
+
+public partial record CraneOperation(int Count, int From, int To)
+{
+    [GeneratedRegex("move (\\d+) from (\\d+) to (\\d+)", RegexOptions.Compiled)]
+    private static partial Regex OperationExtractionRegexp();
+
+    public static readonly Regex ParsingRegexp = OperationExtractionRegexp();
+}
+
+public class CratesStack
+{
+    private readonly IDictionary<int, Stack<char>> _stacks = new Dictionary<int, Stack<char>>();
+
+    public CratesStack(IDictionary<int, Stack<char>> stacks)
+        => _stacks = stacks;
+
+    public void Apply(CraneOperation operation, string crateModel)
+    {
+        var (count, from, to) = operation;
+
+        void CrateMover9000Move()
+        {
+            for (var i = 0; i < count; ++i)
+            {
+                var moved = _stacks[from].Pop();
+                _stacks[to].Push(moved);
+            }
+        }
+
+        void CrateMover9001Move()
+        {
+            var crates = new Stack<char>();
+
+            for (var i = 0; i < count; ++i)
+            {
+                var moved = _stacks[from].Pop();
+                crates.Push(moved);
+            }
+
+            while (crates.Count > 0)
+            {
+                var moved = crates.Pop();
+                _stacks[to].Push(moved);
+            }
+        }
+
+        Action move = crateModel switch
+        {
+            "CrateMover 9000" => CrateMover9000Move,
+            "CrateMover 9001" => CrateMover9001Move,
+            _ => throw new UnreachableException()
+        };
+
+        move();
+    }
+
+    public IEnumerable<char> GetTopCrateOnEachStack()
+        => _stacks.Select(kvp => kvp.Value.Peek());
+}
+
+public class CraneOrdering
+{
+    private readonly CratesStack _crates;
+
+    private readonly IEnumerable<CraneOperation> _operations;
+
+    public CraneOrdering(CratesStack crates, IEnumerable<CraneOperation> operations)
+        => (_crates, _operations) = (crates, operations);
+
+    public string GetTopCratesAfterOperationsUsing(string crateModel = "CrateMover 9000")
+    {
+        foreach (var operation in _operations)
+        {
+            _crates.Apply(operation, crateModel);
+        }
+
+        return string.Join(string.Empty, _crates.GetTopCrateOnEachStack());
+    }
+}
+
+public class Solver : Solver<CraneOrdering, string>
+{
+    protected override string InputPath => "Day05/input.txt";
+
+    public override string PartOne(CraneOrdering input)
+        => input.GetTopCratesAfterOperationsUsing("CrateMover 9000");
+
+    public override string PartTwo(CraneOrdering input)
+        => input.GetTopCratesAfterOperationsUsing("CrateMover 9001");
+
+    public override CraneOrdering ReadInput(string inputPath)
+    {
+        var content = File.ReadAllLines(inputPath);
+
+        var drawnStacks = content
+            .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+            .Reverse()
+            .ToArray();
+
+        IEnumerable<char> GetCratesOnColumn(int column)
+        {
+            // Column is 1-based
+            var indice = column - 1;
+            
+            const int offset = 4;
+            var index = 1 + indice * offset;
+
+            return drawnStacks
+                // Skip the indexes
+                .Skip(1)
+                // Retrieve the crates letter
+                .Aggregate(
+                    new Queue<char>(),
+                    (crates, row) =>
+                    {
+                        var crate = row[index];
+
+                        if (crate != ' ')
+                        {
+                            crates.Enqueue(row[index]);
+                        }
+
+                        return crates;
+                    });
+        }
+
+        var columnCount = drawnStacks.First().Count(char.IsDigit);
+
+        var stacks = Enumerable.Range(1, columnCount)
+            .Select(i => new
+            {
+                Index = i,
+                Crates = GetCratesOnColumn(i),
+            })
+            .ToDictionary(
+                entry => entry.Index,
+                entry => new Stack<char>(entry.Crates));
+
+        var operations = content
+            // Skip the crates stacks
+            .SkipWhile(line => !string.IsNullOrWhiteSpace(line))
+            // Skip the blank line
+            .Skip(1)
+            // Parse the operations
+            .Select(line =>
+            {
+                var groups = CraneOperation.ParsingRegexp.Match(line)
+                    .Groups
+                    .Cast<Group>()
+                    .Skip(1)
+                    .Select(group => int.Parse(group.Value))
+                    .ToArray();
+
+                return new CraneOperation(groups[0], groups[1], groups[2]);
+            });
+
+        return new CraneOrdering(new CratesStack(stacks), operations);
+    }
+}
---
+++ src/2022/Day05/SolverTest.cs
@@ -0,0 +1,53 @@
+﻿namespace _2022.Day05;
+
+public class SolverTest : TestEngine<Solver, CraneOrdering, string>
+{
+#pragma warning disable IDE0051 // Remove unused private members
+    private const string rawInput = @"
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3 
+
+        move 1 from 2 to 1
+        move 3 from 1 to 3
+        move 2 from 2 to 1
+        move 1 from 1 to 2"
+#pragma warning restore IDE0051 // Remove unused private members
+;
+
+    private readonly CratesStack _stacks = new(new Dictionary<int, Stack<char>>()
+    {
+        { 1, new Stack<char>(new[] { 'Z', 'N' }) },
+        { 2, new Stack<char>(new[] { 'M', 'C', 'D' }) },
+        { 3, new Stack<char>(new[] { 'P' }) },
+    });
+
+    private readonly CraneOperation[] _operations =
+    {
+        new(1, 2, 1),
+        new(3, 1, 3),
+        new(2, 2, 1),
+        new(1, 1, 2),
+    };
+
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            Input = new CraneOrdering(_stacks, _operations),
+            Result = "CMZ",
+        },
+        Solution = "FRDSQRRCD",
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        Example = new()
+        {
+            Input = new CraneOrdering(_stacks, _operations),
+            Result = "MCD",
+        },
+        Solution = "HRFTQVWNN",
+    };
+}
