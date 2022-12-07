08a444350fd6d5318587235441ab6c68f352c79d
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-07 20:44:10 +0100

    WiP Day 7 part 1

---
+++ src/2022/Day07/Solver.cs
@@ -0,0 +1,135 @@
+﻿namespace _2022.Day07;
+
+public record Directory(string Name)
+{
+    public Directory? Parent;
+
+    public List<Directory> Children = new();
+
+    public List<File> Files = new();
+
+    private long? _size = null;
+
+    public long ComputeSize()
+    {
+        if (_size is not null)
+        {
+            return _size.Value;
+        }
+
+        _size ??= Files.Sum(file => file.Size)
+            + Children.Sum(folder => folder.ComputeSize());
+
+        return _size.Value;
+    }
+}
+
+public record File(string Name, long Size);
+
+public class Solver : Solver<Directory, long>
+{
+    public Solver() : base("Day07/input.txt") { }
+
+    public override long PartOne(Directory root)
+    {
+        var sizes = new List<long>();
+
+        var toVisit = new Stack<Directory>(new[] { root });
+        var visited = new HashSet<Directory>();
+
+        while (toVisit.Count > 0)
+        {
+            var next = toVisit.Pop();
+
+            if (visited.Contains(next)) continue;
+
+            next.Children.ForEach(toVisit.Push);
+
+            sizes.Add(next.ComputeSize());
+
+            visited.Add(next);
+        }
+
+        return sizes.Where(size => size <= 100_000).Sum();
+    }
+
+    public override long PartTwo(Directory root)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Directory ParseInput(IEnumerable<string> input)
+    {
+        var root = new Directory("/");
+        var currentDirectory = root;
+
+        foreach (var line in input)
+        {
+            var splitted = line.Split(' ');
+            
+            // Command
+            if (line.StartsWith('$'))
+            {
+                var command = splitted[1];
+                var argument = splitted.Length > 2
+                    ? splitted[2]
+                    : null;
+
+                // Noop
+                if (command == "ls")
+                {
+                    continue;
+                }
+                // Move to parent
+                else if (argument == "..")
+                {
+                    currentDirectory = currentDirectory.Parent!;
+                }
+                // Change directory
+                else
+                {
+                    var child = currentDirectory.Children.FirstOrDefault(folder => folder.Name == argument);
+
+                    child ??= new Directory(argument)
+                    {
+                        Parent = currentDirectory,
+                    };
+                    
+                    currentDirectory.Children.Add(child);
+
+                    currentDirectory = child;
+                }
+            }
+            // Listing
+            else
+            {
+                // Directory
+                if (splitted[0] == "dir")
+                {
+                    var name = splitted[1];
+
+                    var directory = currentDirectory.Children.FirstOrDefault(folder => folder.Name == name);
+
+                    directory ??= new Directory(name)
+                    {
+                        Parent = currentDirectory,
+                    };
+                    
+                    currentDirectory.Children.Add(directory);
+                }
+                // File
+                else
+                {
+                    var size = long.Parse(splitted[0]);
+                    var name = splitted[1];
+
+                    var file = new File(name, size);
+
+                    currentDirectory.Files.Add(file);
+                }
+            }
+        }
+
+        return root;
+    }
+}
---
+++ src/2022/Day07/SolverTest.cs
@@ -0,0 +1,102 @@
+﻿namespace _2022.Day07;
+
+
+public class SolverTest : TestEngine<Solver, Directory, long>
+{
+    private readonly Dictionary<string, Directory> _directories = new()
+    {
+        { 
+            "/", 
+            new("/")
+            {
+                Files = new()
+                {
+                    new File("b.txt", 14848514),
+                    new File("c.dat", 8504156),
+                },
+            }
+        },
+        { 
+            "a", 
+            new("a")
+            {
+                Files = new()
+                {
+                    new File("f", 29116),
+                    new File("g", 2557),
+                    new File("h.lst", 62596),
+                },
+            }
+        },
+        { 
+            "e", 
+            new("e")
+            {
+                Files = new()
+                {
+                  new File("i", 584),
+                },
+            }
+        },
+        { 
+            "d", 
+            new("d")
+            {
+                Files = new()
+                {
+                    new File("j", 4060174),
+                    new File("d.log", 8033020),
+                    new File("d.ext", 5626152),
+                    new File("k", 7214296),
+                },
+            }
+        },
+    };
+
+    private Directory GetInput()
+    {
+        _directories["/"].Children = new()
+        {
+            _directories["a"],
+            _directories["d"],
+        };
+
+        _directories["a"].Parent = _directories["/"];
+        _directories["a"].Children = new()
+        {
+            _directories["e"],
+        };
+
+        _directories["e"].Parent = _directories["a"];
+
+        _directories["d"].Parent = _directories["/"];
+
+        return _directories["/"];
+    }
+
+    public override Puzzle PartOne => new()
+    {
+        // TODO - WiP
+        ShouldSkipTests = true,
+
+        Example = new()
+        {
+            Input = GetInput(),
+            Result = 95437,
+        },
+        Solution = 1159091,  // Not: 801867 or 1159091
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        // TODO - WiP
+        ShouldSkipTests = true,
+
+        Example = new()
+        {
+            Input = GetInput(),
+            Result = 0,
+        },
+        Solution = 0,
+    };
+}
