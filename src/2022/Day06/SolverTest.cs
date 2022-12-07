70486e138cd2cae75877142b649c2df53e1dc60d
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-07 08:13:37 +0100

    Add Day 6 part 1 & 2

---
+++ src/2022/Day06/SolverTest.cs
@@ -0,0 +1,24 @@
+﻿namespace _2022.Day06;
+
+public class SolverTest : TestEngine<Solver, string, int>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            Input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+            Result = 7,
+        },
+        Solution = 1275,
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        Example = new()
+        {
+            Input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+            Result = 19,
+        },
+        Solution = 3605,
+    };
+}
