a3a52d5b9e675ecf54c8709874fc72aea60227c0
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-08 21:48:36 +0100

    Add day 8 part 1

---
+++ src/2022/Day08/Solver.cs
@@ -0,0 +1,103 @@
+﻿namespace _2022.Day08;
+
+public static class MatrixExtensions
+{
+    public static int[] GetColumn(this int[,] matrix, int columnNumber)
+        => Enumerable.Range(0, matrix.GetLength(0))
+                .Select(x => matrix[x, columnNumber])
+                .ToArray();
+
+    public static int[] GetRow(this int[,] matrix, int rowNumber)
+        => Enumerable.Range(0, matrix.GetLength(1))
+                .Select(x => matrix[rowNumber, x])
+                .ToArray();
+
+    public static bool AreSmallerThan(this IEnumerable<int> trees, int current)
+        => trees.All(tree => tree < current);
+}
+
+public class Solver : Solver<int[,], int>
+{
+    public Solver() : base("Day08/input.txt") { }
+
+    public override int PartOne(int[,] input)
+    {
+        var visibleTreesCount = 0;
+
+        var forestWidth = input.GetLength(0);
+        for (var i = 0; i < forestWidth; ++i)
+        {
+            var isOnEdge = i == 0 || i == forestWidth - 1;
+
+            if (isOnEdge)
+            {
+                visibleTreesCount += forestWidth;
+                continue;
+            }
+
+            for (var j = 0; j < forestWidth; ++j)
+            {
+                isOnEdge = j == 0 || j == forestWidth - 1;
+
+                if (isOnEdge)
+                {
+                    ++visibleTreesCount;
+                    continue;
+                }
+                
+                var column = input.GetColumn(j);
+                var row = input.GetRow(i);
+
+                var trees = new
+                {
+                    OnTop = column[..i],
+                    OnBottom = column[(i + 1)..],
+                    OnRight = row[(j + 1)..],
+                    OnLeft = row[..j],
+                };
+                
+                var tree = input[i, j];
+
+                var intVisible = trees.OnTop.AreSmallerThan(tree)
+                    || trees.OnRight.AreSmallerThan(tree)
+                    || trees.OnBottom.AreSmallerThan(tree)
+                    || trees.OnLeft.AreSmallerThan(tree);
+
+                if (intVisible)
+                {
+                    ++visibleTreesCount;
+                    continue;
+                }
+            }
+        }
+
+        return visibleTreesCount;
+    }
+
+    public override int PartTwo(int[,] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int[,] ParseInput(IEnumerable<string> input)
+    {
+        var forest = input
+            .Select(row => row.ToCharArray()
+                .Select(x => int.Parse(x.ToString()))
+                .ToArray())
+            .ToArray();
+
+        var length = forest.Length;
+        var treeHeights = new int[length, length];
+
+        for (var i = 0; i < length; ++i)
+        {
+            for (var j = 0; j < length; ++j)
+            {
+                treeHeights[i, j] = forest[i][j];
+            }
+        }
+
+        return treeHeights;
+    }
+}
---
+++ src/2022/Day08/SolverTest.cs
@@ -0,0 +1,34 @@
+﻿namespace _2022.Day08;
+
+public class SolverTest : TestEngine<Solver, int[,], int>
+{
+    private readonly int[,] _forest = new int[,]
+    {
+        { 3, 0, 3, 7, 3 },
+        { 2, 5, 5, 1, 2 },
+        { 6, 5, 3, 3, 2 },
+        { 3, 3, 5, 4, 9 },
+        { 3, 5, 3, 9, 0 },
+    };
+
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            Input = _forest,
+            Result = 21,
+        },
+        Solution = 1880,  // Not 1684 < x < 1880
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        ShouldSkipTests = true,
+        Example = new()
+        {
+            Input = _forest,
+            Result = 21,
+        },
+        Solution = 1851,
+    };
+}
