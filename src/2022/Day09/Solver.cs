a5b1ba119321fa295e25896008cb73614e6e88cd
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-09 08:20:29 +0100

    Day 9, add parsing

---
+++ src/2022/Day09/Solver.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace _2022.Day09;
+
+public enum Direction
+{
+    Right, 
+    Left, 
+    Up, 
+    Down,
+}
+
+public record Motion(Direction Direction, int Times);
+
+public record Coordinate(int X, int Y)
+{
+    public static readonly Coordinate Origin = new(0, 0);
+}
+
+public record Knot(Coordinate Coordinate);
+
+public record Rope
+{
+    public Knot Head { get; } = new Knot(Coordinate.Origin);
+    public Knot Tail { get; } = new Knot(Coordinate.Origin);
+}
+
+public class Solver : Solver<IEnumerable<Motion>, int>
+{
+    public Solver() : base("Day0/input.txt") { }
+
+    public override int PartOne(IEnumerable<Motion> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int PartTwo(IEnumerable<Motion> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<Motion> ParseInput(IEnumerable<string> input)
+        => input.Select(motion =>
+        {
+            var row = motion.Split(' ');
+
+            var direction = row[0] switch
+            {
+                "R" => Direction.Right,
+                "L" => Direction.Left,
+                "U" => Direction.Up,
+                "D" => Direction.Down,
+                _ => throw new UnreachableException(),
+            };
+
+            var times = int.Parse(row[1]);
+
+            return new Motion(direction, times);
+        });
+}
---
+++ src/2022/Day09/SolverTest.cs
@@ -0,0 +1,37 @@
+﻿namespace _2022.Day09;
+
+public class SolverTest : TestEngine<Solver, IEnumerable<Motion>, int>
+{
+    private readonly Motion[] _motions = new Motion[]
+    {
+        new(Direction.Right, 4),
+        new(Direction.Up, 4),
+        new(Direction.Left, 3),
+        new(Direction.Down, 1),
+        new(Direction.Right, 4),
+        new(Direction.Down, 1),
+        new(Direction.Left, 5),
+        new(Direction.Right, 2),
+    };
+
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            Input = _motions,
+            Result = 13,
+        },
+        Solution = 0,
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        ShouldSkipTests = true,
+        Example = new()
+        {
+            Input = _motions,
+            Result = 0,
+        },
+        Solution = 0,
+    };
+}
