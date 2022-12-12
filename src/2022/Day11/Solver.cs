a3beefda672e051d02fd5d6ef80baf91d060c2c0
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-12 20:28:19 +0100

    Add day 11 part 1

---
+++ src/2022/Day11/Solver.cs
@@ -0,0 +1,196 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _2022.Day11;
+
+public record Item(long WorryLevel);
+
+public record Throw(Item Item, int To);
+
+public class Monkey
+{
+    public int InspectedItems { get; private set; } = 0;
+
+    private readonly Queue<Item> _items = new();
+
+    private readonly Func<Item, long> _inspect;
+
+    private readonly Func<Item, int> _throw;
+
+    public Monkey(IEnumerable<Item> items, Func<Item, long> inspect, Func<Item, int> @throw)
+    {
+        _items = new Queue<Item>(items);
+
+        _throw = @throw;
+        _inspect = inspect;
+    }
+
+    public void Receive(Item item)
+        => _items.Enqueue(item);
+
+    public Queue<Throw> PlayTurn()
+    {
+        var thrown = new Queue<Throw>();
+
+        while (_items.Count > 0)
+        {
+            var item = _items.Dequeue();
+
+            item = Inspect(item);
+            item = GetBoredOf(item);
+
+            var @throw = new Throw(item, _throw(item));
+
+            thrown.Enqueue(@throw);
+        }
+
+        return thrown;
+    }
+
+    private Item Inspect(Item item)
+    {
+        ++InspectedItems;
+
+        return item with
+        {
+            WorryLevel = _inspect(item)
+        };
+    }
+
+    private static Item GetBoredOf(Item item)
+        => item with
+        {
+            WorryLevel = (long)Math.Floor(item.WorryLevel / 3d),
+        };
+}
+
+public class Monkeys
+{
+    private readonly Dictionary<int, Monkey> _monkeys = new();
+
+    public long MonkeyBusiness
+        => _monkeys.Values
+            .Select(monkey => monkey.InspectedItems)
+            .OrderByDescending(inspectedItems => inspectedItems)
+            .Take(2)
+            .Aggregate(
+                1L,
+                (count, inspectedItems) => count * inspectedItems);
+
+    public void Add(int id, Monkey monkey)
+        => _monkeys.Add(id, monkey);
+
+    public void PlayRounds(int count)
+        => Enumerable.Range(0, count)
+            .ToList()
+            .ForEach(_ => PlayRound());
+
+    public void PlayRound()
+    {
+        foreach (var (_, monkey) in _monkeys)
+        {
+            var thrown = monkey.PlayTurn();
+
+            foreach (var (item, to) in thrown)
+            {
+                _monkeys[to].Receive(item);
+            }
+        }
+    }
+}
+
+public class Solver : Solver<Monkeys, long>
+{
+    public Solver() : base("Day11/input.txt") { }
+
+    public override long PartOne(Monkeys monkeys)
+    {
+        monkeys.PlayRounds(count: 20);
+        return monkeys.MonkeyBusiness;
+    }
+
+    public override long PartTwo(Monkeys monkeys)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Monkeys ParseInput(IEnumerable<string> input)
+    {
+        const int monkeyDefinitionLenght = 6;
+
+        var monkeyDefinitions = input
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .Select((x, i) => new
+            {
+                Key = i / monkeyDefinitionLenght,
+                Value = x,
+            })
+            .GroupBy(
+                x => x.Key,
+                x => x.Value,
+                (_, group) => group);
+
+        static IEnumerable<long> GetNumbersIn(string line)
+            => Regex.Matches(line, @"\d+")
+                .OfType<Match>()
+                .Select(m => long.Parse(m.Value));
+
+        static (int, Monkey) CreateMonkeyFrom(IEnumerable<string> definition)
+        {
+            var receipe = definition.ToArray();
+
+            // Items
+            var startingItems = GetNumbersIn(receipe[1])
+                .Select(worryLevel => new Item(worryLevel));
+
+            // Inspection
+            var operation = receipe[2].Split(" = ")[1];
+            var parsed = operation.Split(" ");
+
+            Func<Item, long> inspect = (item) =>
+            {
+                var left = parsed[0] == "old"
+                    ? item.WorryLevel
+                    : long.Parse(parsed[0]);
+
+                var right = parsed[2] == "old"
+                    ? item.WorryLevel
+                    : long.Parse(parsed[2]);
+
+                return parsed[1] switch
+                {
+                    "+" => left + right,
+                    "-" => left - right,
+                    "*" => left * right,
+                    "/" => left / right,
+                    _ => throw new ArgumentException(),
+                };
+            };
+
+            // Throw
+            var divisibleBy = (int)GetNumbersIn(receipe[3]).First();
+            var ifTrue = (int)GetNumbersIn(receipe[4]).First();
+            var ifFalse = (int)GetNumbersIn(receipe[5]).First();
+
+            Func<Item, int> @throw = (item) => item.WorryLevel % divisibleBy == 0 
+                ? ifTrue
+                : ifFalse;
+
+            // Result
+            var id = (int)GetNumbersIn(receipe[0]).First();
+            var monkey = new Monkey(startingItems, inspect, @throw);
+
+            return (id, monkey);
+        }
+
+        var createdMonkeys = monkeyDefinitions.Select(CreateMonkeyFrom);
+
+        var monkeys = new Monkeys();
+
+        foreach (var (id, monkey) in createdMonkeys)
+        {
+            monkeys.Add(id, monkey);
+        }
+
+        return monkeys;
+    }
+}
---
+++ src/2022/Day11/SolverTest.cs
@@ -0,0 +1,62 @@
+﻿namespace _2022.Day11;
+
+public class SolverTest : TestEngine<Solver, Monkeys, long>
+{
+    private readonly Monkey[] _monkeys = new[]
+    {
+        new Monkey(
+            items: new Item[] { new(79), new(98) },
+            inspect: (item) => item.WorryLevel * 19,
+            @throw: (item) => item.WorryLevel % 23 == 0 ? 2 : 3),
+
+        new Monkey(
+            items: new Item[] { new(54), new(65), new(75), new(74) },
+            inspect: (item) => item.WorryLevel + 6,
+            @throw: (item) => item.WorryLevel % 19 == 0 ? 2 : 0),
+
+        new Monkey(
+            items: new Item[] { new(79), new(60), new(97) },
+            inspect: (item) => item.WorryLevel * item.WorryLevel,
+            @throw: (item) => item.WorryLevel % 13 == 0 ? 1 : 3),
+
+        new Monkey(
+            items: new Item[] { new(74) },
+            inspect: (item) => item.WorryLevel + 3,
+            @throw: (item) => item.WorryLevel % 17 == 0 ? 0 : 1),
+    };
+
+    private Monkeys GetMonkeys()
+    {
+        var monkeys = new Monkeys();
+
+        for (var i = 0; i < _monkeys.Length; ++i)
+        {
+            monkeys.Add(i, _monkeys[i]);
+        }
+
+        return monkeys;
+    }
+
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            Input = GetMonkeys(),
+            Result = 10605,
+        },
+        Solution = 62491,
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        // Still yet to be done
+        ShouldSkipTests= true,
+
+        Example = new()
+        {
+            Input = GetMonkeys(),
+            Result = 10605,
+        },
+        Solution = 62491,
+    };
+}
