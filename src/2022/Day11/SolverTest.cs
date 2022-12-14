76f90cc0f62805e1eb64654ebe6f363a53aa4f19
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-12-14 16:42:53 +0100

    Add day 11 part 2

--- src/2022/Day11/SolverTest.cs
+++ src/2022/Day11/SolverTest.cs
@@ -2,26 +2,28 @@
 
 public class SolverTest : TestEngine<Solver, Monkeys, ulong>
 {
+    private const ulong MonkeyDivider = 23 * 19 * 13 * 17;
+
     private readonly Monkey[] _monkeys = new[]
     {
         new Monkey(
             items: new Item[] { new(79), new(98) },
-            inspect: (item) => item.WorryLevel * 19,
+            inspect: (item) => item.WorryLevel * 19 % MonkeyDivider,
             @throw: (item) => item.WorryLevel % 23 == 0 ? 2 : 3),
 
         new Monkey(
             items: new Item[] { new(54), new(65), new(75), new(74) },
-            inspect: (item) => item.WorryLevel + 6,
+            inspect: (item) => item.WorryLevel + 6 % MonkeyDivider,
             @throw: (item) => item.WorryLevel % 19 == 0 ? 2 : 0),
 
         new Monkey(
             items: new Item[] { new(79), new(60), new(97) },
-            inspect: (item) => item.WorryLevel * item.WorryLevel,
+            inspect: (item) => item.WorryLevel * item.WorryLevel % MonkeyDivider,
             @throw: (item) => item.WorryLevel % 13 == 0 ? 1 : 3),
 
         new Monkey(
             items: new Item[] { new(74) },
-            inspect: (item) => item.WorryLevel + 3,
+            inspect: (item) => item.WorryLevel + 3 % MonkeyDivider,
             @throw: (item) => item.WorryLevel % 17 == 0 ? 0 : 1),
     };
 
@@ -42,21 +44,18 @@ public class SolverTest : TestEngine<Solver, Monkeys, ulong>
         Example = new()
         {
             Input = GetMonkeys(),
-            Result = 10605uL,
+            Result = 10605,
         },
-        Solution = 62491uL,
+        Solution = 62491,
     };
 
     public override Puzzle PartTwo => new()
     {
-        // WiP
-        ShouldSkipTests = true,
-
         Example = new()
         {
             Input = GetMonkeys(),
-            Result = 2713310158uL,
+            Result = 2713310158,
         },
-        Solution = 0,
+        Solution = 17408399184,
     };
 }
