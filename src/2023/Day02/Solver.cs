1a48fec8af1f9d429c02529f28c1504d60c2750a
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-02 09:32:29 +0100

    Day 2 - Add parsing

---
+++ src/2023/Day02/Solver.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _2023.Day02;
+
+public class Solver : Solver<Game[], long>
+{
+    public Solver() : base("Day02/input.txt") { }
+
+    public override long PartOne(Game[] games)
+        => 0;
+
+    public override long PartTwo(Game[] games)
+        => 0;
+
+    public override Game[] ParseInput(IEnumerable<string> input)
+        => input.Select(line =>
+        {
+            var gameId = Regex.Match(line, @"Game (\d+): ")
+                .Groups
+                .Cast<Group>()
+                .Skip(1)
+                .Select(group => int.Parse(group.Value))
+                .Single();
+
+            var hands = line.Split(';')
+                .Select(Hand.From)
+                .ToArray();
+
+            return new Game(gameId, hands);
+        })
+        .ToArray();
+}
+
+public record Hand(int RedCount, int GreenCount, int BlueCount)
+{
+    public static Hand From(string hand)
+    {
+        var redCount = Regex.Match(hand, @"(\d+) red")
+                .Groups
+                .Cast<Group>()
+                .Select(group => int.Parse(group.Value))
+                .Skip(1)
+                .FirstOrDefault();
+
+        var greenCount = Regex.Match(hand, @"(\d+) green")
+                .Groups
+                .Cast<Group>()
+                .Select(group => int.Parse(group.Value))
+                .Skip(1)
+                .FirstOrDefault();
+
+        var blueCount = Regex.Match(hand, @"(\d+) blue")
+                .Groups
+                .Cast<Group>()
+                .Select(group => int.Parse(group.Value))
+                .Skip(1)
+                .FirstOrDefault();
+
+        return new Hand(
+            RedCount: redCount,
+            GreenCount: greenCount,
+            BlueCount: blueCount);
+    }
+}
+
+public record Game(int Id, Hand[] Hands);
---
+++ src/2023/Day02/SolverTest.cs
@@ -0,0 +1,49 @@
+﻿namespace _2023.Day02;
+
+public class SolverTest : TestEngine<Solver, Game[], long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new Example
+        {
+            Input = [
+                new Game(1, [
+                    new Hand(RedCount: 4, GreenCount: 0, BlueCount: 3),
+                    new Hand(RedCount: 1, GreenCount: 2, BlueCount: 6),
+                    new Hand(RedCount: 0, GreenCount: 2, BlueCount: 0),
+                ]),
+                new Game(2, [
+                    new Hand(RedCount: 0, GreenCount: 2, BlueCount: 1),
+                    new Hand(RedCount: 1, GreenCount: 3, BlueCount: 4),
+                    new Hand(RedCount: 0, GreenCount: 1, BlueCount: 1),
+                ]),
+                new Game(3, [
+                    new Hand(RedCount: 20, GreenCount: 8, BlueCount: 6),
+                    new Hand(RedCount: 4, GreenCount: 13, BlueCount: 5),
+                    new Hand(RedCount: 1, GreenCount: 5, BlueCount: 0),
+                ]),
+                new Game(4, [
+                    new Hand(RedCount: 3, GreenCount: 1, BlueCount: 6),
+                    new Hand(RedCount: 6, GreenCount: 3, BlueCount: 0),
+                    new Hand(RedCount: 14, GreenCount: 3, BlueCount: 15),
+                ]),
+                new Game(5, [
+                    new Hand(RedCount: 6, GreenCount: 3, BlueCount: 1),
+                    new Hand(RedCount: 1, GreenCount: 2, BlueCount: 2),
+                ]),
+            ],
+            Result = 0,
+        },
+        Solution = 0,
+    };
+
+    public override Puzzle PartTwo => new()
+    {
+        Example = new Example
+        {
+            Input = Array.Empty<Game>(),
+            Result = 0,
+        },
+        Solution = 0,
+    };
+}
