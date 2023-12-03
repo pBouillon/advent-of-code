53860b0ece71a8db3e9760dbcfc066a14adb8a9b
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-03 08:53:23 +0100

    Add day 3 part 1

---
+++ src/2023/Day03/Solver.cs
@@ -0,0 +1,121 @@
+﻿namespace _2023.Day03;
+
+public class Solver : Solver<EngineSchematic, long>
+{
+    public Solver() : base("Day03/input.txt") { }
+
+    public override long PartOne(EngineSchematic input)
+        => input.PartNumbers.Sum();
+
+    public override long PartTwo(EngineSchematic input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override EngineSchematic ParseInput(IEnumerable<string> input)
+    {
+        var symbols = new List<Coordinate>();
+
+        var rows = input.ToArray();
+
+        // Symbols Lookup
+        for (var y = 0; y < rows.Length; ++y)
+        {
+            for (var x = 0; x < rows[y].Length; ++x)
+            {
+                var value = rows[y][x];
+
+                if (!EngineSchematic.IsSymbol(value)) continue;
+
+                var coordinate = new Coordinate(x, y);
+                symbols.Add(coordinate);
+            }
+        }
+
+        var engine = new EngineSchematic(symbols);
+
+        // Engine Parts Lookup
+        Coordinate? partStart;
+        var buffer = string.Empty;
+
+        void Reset()
+        {
+            buffer = string.Empty;
+            partStart = null;
+        }
+
+        for (var y = 0; y < rows.Length; ++y)
+        {
+            Reset();
+
+            for (var x = 0; x < rows[y].Length; ++x)
+            {
+                var value = rows[y][x];
+
+                // Reading an engine part
+                if (EngineSchematic.MightBePart(value))
+                {
+                    partStart ??= new Coordinate(x, y);
+                    buffer += value;
+                    continue;
+                }
+
+                // Finished reading an engine part
+                if (partStart is not null)
+                {
+                    // Create the part
+                    var part = new Part(
+                        From: partStart,
+                        Length: buffer.Length,
+                        Value: long.Parse(buffer));
+
+                    engine.EvaluateAndAdd(part);
+
+                    Reset();
+                }
+            }
+        }
+
+        return engine;
+    }
+}
+
+public record Coordinate(int X, int Y)
+{
+    public bool IsAdjascentTo(Coordinate coordinate)
+    {
+        int deltaX = Math.Abs(coordinate.X - X);
+        int deltaY = Math.Abs(coordinate.Y - Y);
+
+        return deltaX <= 1 && deltaY <= 1;
+    }
+}
+
+
+public record Part(Coordinate From, int Length, long Value)
+{
+    public bool IsNextToAny(List<Coordinate> coordinates)
+        => Enumerable.Range(0, Length)
+            .Select(offset => From with { X = From.X + offset })
+            .Any(coordinate => coordinates.Any(coordinate.IsAdjascentTo));
+}
+
+public class EngineSchematic(List<Coordinate> SymbolCoordinates)
+{
+    public List<long> PartNumbers { get; init; } = new();
+
+    public static bool MightBePart(char @char)
+        => char.IsDigit(@char);
+
+    public static bool IsSymbol(char @char)
+        => !char.IsDigit(@char) && @char != '.';
+
+    public void EvaluateAndAdd(Part part)
+    {
+        var isNextToSymbol = part.IsNextToAny(SymbolCoordinates);
+        if (isNextToSymbol)
+        {
+            PartNumbers.Add(part.Value);
+        }
+    }
+}
---
+++ src/2023/Day03/SolverTest.cs
@@ -0,0 +1,38 @@
+﻿namespace _2023.Day03;
+
+public class SolverTest : TestEngine<Solver, EngineSchematic, long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            RawInput = [
+                "467..114..",
+                "...*......", // (1, 3)
+                "..35..633.",
+                "......#...", // (3, 6)
+                "617*......", // (4, 3)
+                ".....+.58.", // (5, 5)
+                "..592.....",
+                "......755.",
+                "...$.*....", // (8, 3), (8, 5)
+                ".664.598.."
+                ],
+            Input = new EngineSchematic([
+                new(X: 1, Y: 3),
+                new(X: 4, Y: 3),
+                new(X: 8, Y: 3),
+                new(X: 5, Y: 5),
+                new(X: 8, Y: 5),
+                new(X: 3, Y: 6),
+            ])
+            {
+                PartNumbers = [467, 35, 633, 617, 592, 755, 664, 598],
+            },
+            Result = 4_361,
+        },
+        Solution = 556_057,
+    };
+
+    public override Puzzle PartTwo => throw new NotImplementedException();
+}
