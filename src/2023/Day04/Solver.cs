a02c193adff9d5e46a8cf7b1c5a1c1d7f72f610f
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-04 06:35:50 +0100

    Solve day 4 part 1

---
+++ src/2023/Day04/Solver.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace _2023.Day04;
+
+public class Solver : Solver<ScratchCard[], long>
+{
+    public Solver() : base("day04/input.txt") { }
+
+    public override long PartOne(ScratchCard[] input)
+        => input.Sum(card => card.Points);
+
+    public override long PartTwo(ScratchCard[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ScratchCard[] ParseInput(IEnumerable<string> input)
+        => input
+            .Select(card =>
+            {
+                var cardId = Regex
+                    .Match(card, @"Card\s+(?<id>\d+):")
+                    .Groups["id"]
+                    .Value;
+
+                var numbersIndex = card.LastIndexOf(':') + 1;
+                var rawNumbers = card[(numbersIndex + 1)..].Split('|');
+
+                var scratchedNumbers = rawNumbers[1]
+                    .Split(' ')
+                    .Where(x => x.Length > 0)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var winningNumbers = rawNumbers[0]
+                    .Split(' ')
+                    .Where(x => x.Length > 0)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                return new ScratchCard
+                {
+                    Id = int.Parse(cardId),
+                    ScratchedNumbers = scratchedNumbers,
+                    WinningNumbers = winningNumbers,
+                };
+            })
+            .ToArray();
+}
+
+public class ScratchCard
+{
+    public required int Id { get; init; }
+    public required IReadOnlyList<int> ScratchedNumbers { get; init; } = [];
+    public required IReadOnlyList<int> WinningNumbers { get; init; } = [];
+
+    public long Points => (long)Math.Pow(
+        2,
+        ScratchedNumbers.Intersect(WinningNumbers).Count() - 1);
+}
---
+++ src/2023/Day04/SolverTest.cs
@@ -0,0 +1,61 @@
+﻿namespace _2023.Day04;
+
+public class SolverTest : TestEngine<Solver, ScratchCard[], long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            RawInput = [
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            ],
+            Input = [
+                new ScratchCard
+                {
+                    Id = 1,
+                    ScratchedNumbers = [83, 86, 6, 31, 17, 9, 48, 53],
+                    WinningNumbers = [41, 48, 83, 86, 17],
+                },
+                new ScratchCard
+                {
+                    Id = 2,
+                    ScratchedNumbers = [61, 30, 68, 82, 17, 32, 24, 19],
+                    WinningNumbers = [13, 32, 20, 16, 61],
+                },
+                new ScratchCard
+                {
+                    Id = 3,
+                    ScratchedNumbers = [69, 82, 63, 72, 16, 21, 14, 1],
+                    WinningNumbers = [1, 21, 53, 59, 44],
+                },
+                new ScratchCard
+                {
+                    Id = 4,
+                    ScratchedNumbers = [59, 84, 76, 51, 58, 5, 54, 83],
+                    WinningNumbers = [41, 92, 73, 84, 69],
+                },
+                new ScratchCard
+                {
+                    Id = 5,
+                    ScratchedNumbers = [88, 30, 70, 12, 93, 22, 82, 36],
+                    WinningNumbers = [87, 83, 26, 28, 32],
+                },
+                new ScratchCard
+                {
+                    Id = 6,
+                    ScratchedNumbers = [74, 77, 10, 23, 35, 67, 36, 11],
+                    WinningNumbers = [31, 18, 13, 56, 72],
+                },
+            ],
+            Result = 13
+        },
+        Solution = 21_558,
+    };
+
+    public override Puzzle PartTwo => throw new NotImplementedException();
+}
