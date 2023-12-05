c42f8e330cfa451bc59a181a64277c3af8460657
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-05 07:26:03 +0100

    Solve day 5 part 1

---
+++ src/2023/Day05/Solver.cs
@@ -0,0 +1,150 @@
+﻿
+using FluentAssertions;
+
+using System.Text.RegularExpressions;
+
+namespace _2023.Day05;
+
+public class Solver : Solver<Almanac, long>
+{
+    public Solver() : base("Day05/input.txt") { }
+
+    public override long PartOne(Almanac almanac)
+    {
+        long GetLocationOfSeed(long seed)
+        {
+            var soil = almanac.SeedToSoilMap.Translate(seed);
+            var fertilizer = almanac.SoilToFertilizerMap.Translate(soil);
+            var water = almanac.FertilizerToWaterMap.Translate(fertilizer);
+            var light = almanac.WaterToLightMap.Translate(water);
+            var temperature = almanac.LightToTemperatureMap.Translate(light);
+            var humidity = almanac.TemperatureToHumidityMap.Translate(temperature);
+            var location = almanac.HumidityToLocationMap.Translate(humidity);
+
+            return location;
+        }
+
+        var seedLocations = almanac.SeedsToPlant.ToDictionary(
+            seed => seed,
+            GetLocationOfSeed);
+
+        return seedLocations.Values.Min();
+    }
+
+    public override long PartTwo(Almanac almanac)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Almanac ParseInput(IEnumerable<string> input)
+    {
+        var instuctions = input.ToList();
+
+        var seeds = Regex.Matches(instuctions.First(), @"\d+")
+            .Cast<Match>()
+            .Select(match => long.Parse(match.Value))
+            .ToArray();
+
+        var seedToSoilMap = instuctions
+            .SkipWhile(line => !line.Contains("seed-to-soil"))
+            .Skip(1)
+            .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line =>
+            {
+                var range = Regex.Matches(line, @"\d+")
+                    .Cast<Match>()
+                    .Select(match => long.Parse(match.Value))
+                    .ToArray();
+
+                return new Range(range[0], range[1], range[2]);
+            })
+            .ToArray();
+
+        return new Almanac
+        {
+            SeedsToPlant = seeds,
+            SeedToSoilMap = new()
+            {
+                Ranges = GetRangesFor("seed-to-soil"),
+            },
+            SoilToFertilizerMap = new()
+            {
+                Ranges = GetRangesFor("soil-to-fertilizer"),
+            },
+            FertilizerToWaterMap = new()
+            {
+                Ranges = GetRangesFor("fertilizer-to-water"),
+            },
+            WaterToLightMap = new()
+            {
+                Ranges = GetRangesFor("water-to-light"),
+            },
+            LightToTemperatureMap = new()
+            {
+                Ranges = GetRangesFor("light-to-temperature"),
+            },
+            TemperatureToHumidityMap = new()
+            {
+                Ranges = GetRangesFor("temperature-to-humidity"),
+            },
+            HumidityToLocationMap = new()
+            {
+                Ranges = GetRangesFor("humidity-to-location"),
+            },
+        };
+
+        Range[] GetRangesFor(string header)
+            => instuctions
+                .SkipWhile(line => !line.Contains(header))
+                .Skip(1)
+                .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line =>
+                {
+                    var range = Regex.Matches(line, @"\d+")
+                        .Cast<Match>()
+                        .Select(match => long.Parse(match.Value))
+                        .ToArray();
+
+                    return new Range(range[0], range[1], range[2]);
+                })
+                .ToArray();
+    }
+}
+
+public class Almanac
+{
+    public required long[] SeedsToPlant { get; init; } = [];
+    public required AlmanacMap SeedToSoilMap { get; init; }
+    public required AlmanacMap SoilToFertilizerMap { get; init; }
+    public required AlmanacMap FertilizerToWaterMap { get; init; }
+    public required AlmanacMap WaterToLightMap { get; init; }
+    public required AlmanacMap LightToTemperatureMap { get; init; }
+    public required AlmanacMap TemperatureToHumidityMap { get; init; }
+    public required AlmanacMap HumidityToLocationMap { get; init; }
+}
+
+public class AlmanacMap
+{
+    public required Range[] Ranges = [];
+
+    public long Translate(long target)
+    {
+        var range = Ranges
+            .Where(range => range.IsMapping(target))
+            .FirstOrDefault();
+
+        if (range is null) return target;
+
+        var offset = target - range.SourceStart;
+        return range.DestinationStart + offset;
+    }
+}
+
+public record Range(long DestinationStart, long SourceStart, long Length)
+{
+    public long Offset = Math.Abs(SourceStart - DestinationStart);
+
+    public bool IsMapping(long target)
+        => target > SourceStart
+            && target - SourceStart <= Length;
+}
---
+++ src/2023/Day05/SolverTest.cs
@@ -0,0 +1,107 @@
+﻿namespace _2023.Day05;
+
+public class SolverTest : TestEngine<Solver, Almanac, long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+            RawInput = [
+                "seeds: 79 14 55 13",
+                "",
+                "seed-to-soil map:",
+                "50 98 2",
+                "52 50 48",
+                "",
+                "soil-to-fertilizer map:",
+                "0 15 37",
+                "37 52 2",
+                "39 0 15",
+                "",
+                "fertilizer-to-water map:",
+                "49 53 8",
+                "0 11 42",
+                "42 0 7",
+                "57 7 4",
+                "",
+                "water-to-light map:",
+                "88 18 7",
+                "18 25 70",
+                "",
+                "light-to-temperature map:",
+                "45 77 23",
+                "81 45 19",
+                "68 64 13",
+                "",
+                "temperature-to-humidity map:",
+                "0 69 1",
+                "1 0 69",
+                "",
+                "humidity-to-location map:",
+                "60 56 37",
+                "56 93 4",
+            ],
+            Input = new Almanac
+            {
+                SeedsToPlant = [79, 14, 55, 13],
+                SeedToSoilMap = new()
+                {
+                    Ranges = [
+                        new(50, 98, 2),
+                        new(52, 50, 48),
+                    ],
+                },
+                SoilToFertilizerMap = new()
+                {
+                    Ranges = [
+                        new(0, 15, 37),
+                        new(37, 52, 2),
+                        new(39, 0, 15),
+                    ],
+                },
+                FertilizerToWaterMap = new()
+                {
+                    Ranges = [
+                        new(49, 53, 8),
+                        new(0, 11, 42),
+                        new(42, 0, 7),
+                        new(57, 7, 4),
+                    ],
+                },
+                WaterToLightMap = new()
+                {
+                    Ranges = [
+                        new(88, 18, 7),
+                        new(18, 25, 70),
+                    ],
+                },
+                LightToTemperatureMap = new()
+                {
+                    Ranges = [
+                        new(45, 77, 23),
+                        new(81, 45, 19),
+                        new(68, 64, 13),
+                    ],
+                },
+                TemperatureToHumidityMap = new()
+                {
+                    Ranges = [
+                        new(0, 69, 1),
+                        new(1, 0, 69),
+                    ],
+                },
+                HumidityToLocationMap = new()
+                {
+                    Ranges = [
+                        new(60, 56, 37),
+                        new(56, 93, 4),
+                    ],
+                },
+            },
+            Result = 35,
+        },
+        Solution = 806_029_445,
+    };
+
+    public override Puzzle PartTwo => throw new NotImplementedException();
+}
