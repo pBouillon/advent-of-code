758cc78bc3e692cb7c7f6e283398cbd5d120608e
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-06 06:23:52 +0100

    Add day 6 parsing

---
+++ src/2023/Day06/Solver.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace _2023.Day06;
+
+public class Solver : Solver<Race[], long>
+{
+    public Solver() : base("Day06/input.txt") { }
+
+    public override long PartOne(Race[] races)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long PartTwo(Race[] races)
+    {
+        throw new NotImplementedException();
+    }
+    public override Race[] ParseInput(IEnumerable<string> input)
+    {
+        var document = input.ToArray();
+
+        var times = GetNumbers(document[0]);
+        var distances = GetNumbers(document[1]);
+
+        return times.Zip(
+                distances,
+                (time, distance) => new Race(time, distance))
+            .ToArray();
+
+        static int[] GetNumbers(string line)
+            => Regex.Matches(line, @"\d+")
+                .Cast<Match>()
+                .Select(match => int.Parse(match.Value))
+                .ToArray();
+    }
+}
+
+public record Race(int Time, int BestDistance);
---
+++ src/2023/Day06/SolverTest.cs
@@ -0,0 +1,25 @@
+﻿namespace _2023.Day06;
+
+public class SolverTest : TestEngine<Solver, Race[], long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new()
+        {
+          RawInput = [
+              "Time:      7  15   30",
+              "Distance:  9  40  200",
+          ],
+          Input = [
+              new Race(Time: 7, BestDistance: 9),
+              new Race(Time: 15, BestDistance: 40),
+              new Race(Time: 30, BestDistance: 200),
+          ],
+          Result = 288,
+        },
+        Solution = 0,
+    };
+
+    public override Puzzle PartTwo => throw new NotImplementedException();
+}
+
