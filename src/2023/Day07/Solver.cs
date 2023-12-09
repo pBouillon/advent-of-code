8df1fca7b011be26fd07b9e7e22d8354c6ae4d4e
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-09 17:42:30 +0100

    Add day 7 part 2

--- src/2023/Day07/Solver.cs
+++ src/2023/Day07/Solver.cs
@@ -11,9 +11,10 @@ public class Solver : Solver<Hand[], long>
             .Sum();
 
     public override long PartTwo(Hand[] hands)
-    {
-        throw new NotImplementedException();
-    }
+        => hands
+            .OrderBy(hand => hand, new HandWithJokerComparer())
+            .Select((hand, rank) => hand.Bid * (rank + 1))
+            .Sum();
 
     public override Hand[] ParseInput(IEnumerable<string> input)
         => input
@@ -29,13 +30,35 @@ public class HandComparer : IComparer<Hand>
 {
     public int Compare(Hand? x, Hand? y)
     {
-        var strengthDifference = y!.HandType.CompareTo(x!.HandType);
+        var strengthDifference = GetHandType(y!)
+            .CompareTo(GetHandType(x!));
+
         if (strengthDifference != 0) return strengthDifference;
 
-        return x.Cards.Select(CardValue)
-            .Zip(y.Cards.Select(CardValue))
+        return x!.Cards.Select(CardValue)
+            .Zip(y!.Cards.Select(CardValue))
             .Select((values) => values.First - values.Second)
-            .First(diff => diff != 0);
+            .FirstOrDefault(diff => diff != 0);
+    }
+
+    private static HandType GetHandType(Hand hand)
+    {
+        var cardFrequency = hand.Cards
+            .GroupBy(card => card)
+            .Select(group => group.Count())
+            .OrderByDescending(card => card)
+            .ToArray();
+
+        return cardFrequency switch
+        {
+            [5] => HandType.FiveOfAKind,
+            [4, 1] => HandType.FourOfAKind,
+            [3, 2] => HandType.FullHouse,
+            [3, 1, 1] => HandType.ThreeOfAKind,
+            [2, 2, 1] => HandType.TwoPair,
+            [2, 1, 1, 1] => HandType.OnePair,
+            _ => HandType.HighCard
+        };
     }
 
     private int CardValue(char card)
@@ -51,19 +74,34 @@ public class HandComparer : IComparer<Hand>
         };
 }
 
-public class Hand(string cards, long bid)
+public class HandWithJokerComparer : IComparer<Hand>
 {
-    public string Cards { get; init; } = cards;
-    public long Bid { get; init; } = bid;
-    public HandType HandType { get; init; } = GetHandType(cards);
-
-    private static HandType GetHandType(string cards)
+    public int Compare(Hand? x, Hand? y)
     {
-        var cardFrequency = cards
+        var strengthDifference = GetHandType(y!)
+            .CompareTo(GetHandType(x!));
+
+        if (strengthDifference != 0) return strengthDifference;
+
+        return x!.Cards.Select(CardValue)
+            .Zip(y!.Cards.Select(CardValue))
+            .Select((values) => values.First - values.Second)
+            .FirstOrDefault(diff => diff != 0);
+    }
+
+    private static HandType GetHandType(Hand hand)
+    {
+        var jokersCount = hand.Cards.Count(card => card == 'J');
+        if (jokersCount == 5) return HandType.FiveOfAKind;
+
+        var cardFrequency = hand.Cards
+            .Where(card => card != 'J')
             .GroupBy(card => card)
             .Select(group => group.Count())
             .OrderByDescending(card => card)
-            .ToList();
+            .ToArray();
+
+        cardFrequency[0] += jokersCount;
 
         return cardFrequency switch
         {
@@ -77,8 +115,23 @@ public class Hand(string cards, long bid)
         };
     }
 
-    public override string ToString()
-        => Cards;
+    private int CardValue(char card)
+        => card switch
+        {
+            'J' => 0,
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'T' => 10,
+            var digit when char.IsDigit(card) => int.Parse(digit.ToString()),
+            _ => 0,
+        };
+}
+
+public class Hand(string cards, long bid)
+{
+    public string Cards { get; init; } = cards;
+    public long Bid { get; init; } = bid;
 }
 
 public enum HandType
--- src/2023/Day07/SolverTest.cs
+++ src/2023/Day07/SolverTest.cs
@@ -14,20 +14,6 @@ public class SolverTest : TestEngine<Solver, Hand[], long>
                 "QQQJA 483",
             ],
             Input = [
-                //new Hand("AAAAA", 1),
-                //new Hand("KKKKK", 1),
-                //new Hand("QQQQQ", 1),
-                //new Hand("JJJJJ", 1),
-                //new Hand("TTTTT", 1),
-                //new Hand("99999", 1),
-                //new Hand("88888", 1),
-                //new Hand("77777", 1),
-                //new Hand("66666", 1),
-                //new Hand("55555", 1),
-                //new Hand("44444", 1),
-                //new Hand("33333", 1),
-                //new Hand("22222", 1),
-                //new Hand("KKKK1", 1),
                 new Hand("32T3K", 765),
                 new Hand("T55J5", 684),
                 new Hand("KK677", 28),
@@ -36,8 +22,22 @@ public class SolverTest : TestEngine<Solver, Hand[], long>
             ],
             Result = 6_440,
         },
-        Solution = 245_794_640,
+        Solution = 248_422_077,
     };
 
-    public override Puzzle PartTwo => throw new NotImplementedException();
+    public override Puzzle PartTwo => new()
+    {
+        Example = new Example
+        {
+            Input = [
+                new Hand("32T3K", 765),
+                new Hand("T55J5", 684),
+                new Hand("KK677", 28),
+                new Hand("KTJJT", 220),
+                new Hand("QQQJA", 483),
+            ],
+            Result = 5_905,
+        },
+        Solution = 0,
+    };
 }
