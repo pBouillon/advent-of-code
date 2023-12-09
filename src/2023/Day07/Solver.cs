cbda3b5457de5911ef669d64e310258127a03d9f
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-09 17:29:06 +0100

    Add day 7 part 1

---
+++ src/2023/Day07/Solver.cs
@@ -0,0 +1,93 @@
+﻿namespace _2023.Day07;
+
+public class Solver : Solver<Hand[], long>
+{
+    public Solver() : base("Day07/input.txt") { }
+
+    public override long PartOne(Hand[] hands)
+        => hands
+            .OrderBy(hand => hand, new HandComparer())
+            .Select((hand, rank) => hand.Bid * (rank + 1))
+            .Sum();
+
+    public override long PartTwo(Hand[] hands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Hand[] ParseInput(IEnumerable<string> input)
+        => input
+            .Select(line =>
+            {
+                var parts = line.Split(' ');
+                return new Hand(parts[0], long.Parse(parts[1]));
+            })
+            .ToArray();
+}
+
+public class HandComparer : IComparer<Hand>
+{
+    public int Compare(Hand? x, Hand? y)
+    {
+        var strengthDifference = y!.HandType.CompareTo(x!.HandType);
+        if (strengthDifference != 0) return strengthDifference;
+
+        return x.Cards.Select(CardValue)
+            .Zip(y.Cards.Select(CardValue))
+            .Select((values) => values.First - values.Second)
+            .First(diff => diff != 0);
+    }
+
+    private int CardValue(char card)
+        => card switch
+        {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            var digit when char.IsDigit(card) => int.Parse(digit.ToString()),
+            _ => 0,
+        };
+}
+
+public class Hand(string cards, long bid)
+{
+    public string Cards { get; init; } = cards;
+    public long Bid { get; init; } = bid;
+    public HandType HandType { get; init; } = GetHandType(cards);
+
+    private static HandType GetHandType(string cards)
+    {
+        var cardFrequency = cards
+            .GroupBy(card => card)
+            .Select(group => group.Count())
+            .OrderByDescending(card => card)
+            .ToList();
+
+        return cardFrequency switch
+        {
+            [5] => HandType.FiveOfAKind,
+            [4, 1] => HandType.FourOfAKind,
+            [3, 2] => HandType.FullHouse,
+            [3, 1, 1] => HandType.ThreeOfAKind,
+            [2, 2, 1] => HandType.TwoPair,
+            [2, 1, 1, 1] => HandType.OnePair,
+            _ => HandType.HighCard
+        };
+    }
+
+    public override string ToString()
+        => Cards;
+}
+
+public enum HandType
+{
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+}
---
+++ src/2023/Day07/SolverTest.cs
@@ -0,0 +1,43 @@
+﻿namespace _2023.Day07;
+
+public class SolverTest : TestEngine<Solver, Hand[], long>
+{
+    public override Puzzle PartOne => new()
+    {
+        Example = new Example
+        {
+            RawInput = [
+                "32T3K 765",
+                "T55J5 684",
+                "KK677 28",
+                "KTJJT 220",
+                "QQQJA 483",
+            ],
+            Input = [
+                //new Hand("AAAAA", 1),
+                //new Hand("KKKKK", 1),
+                //new Hand("QQQQQ", 1),
+                //new Hand("JJJJJ", 1),
+                //new Hand("TTTTT", 1),
+                //new Hand("99999", 1),
+                //new Hand("88888", 1),
+                //new Hand("77777", 1),
+                //new Hand("66666", 1),
+                //new Hand("55555", 1),
+                //new Hand("44444", 1),
+                //new Hand("33333", 1),
+                //new Hand("22222", 1),
+                //new Hand("KKKK1", 1),
+                new Hand("32T3K", 765),
+                new Hand("T55J5", 684),
+                new Hand("KK677", 28),
+                new Hand("KTJJT", 220),
+                new Hand("QQQJA", 483),
+            ],
+            Result = 6_440,
+        },
+        Solution = 245_794_640,
+    };
+
+    public override Puzzle PartTwo => throw new NotImplementedException();
+}
