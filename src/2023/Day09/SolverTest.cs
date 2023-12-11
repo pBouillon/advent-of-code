6da0e2c86cbfef341a13539a4cf3fa4fbc2d73d4
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-11 19:12:34 +0100

    Add day 9 part 2 example

--- src/2023/Day09/SolverTest.cs
+++ src/2023/Day09/SolverTest.cs
@@ -21,5 +21,17 @@ public class SolverTest : TestEngine<Solver, Report, long>
         Solution = 1_898_776_583,
     };
 
-    public override Puzzle PartTwo => throw new NotImplementedException();
+    public override Puzzle PartTwo => new()
+    {
+        Example = new()
+        {
+            Input = new Report([
+                new History([0, 3, 6, 9, 12, 15]),
+                new History([1, 3, 6, 10, 15, 21]),
+                new History([10, 13, 16, 21, 30, 45]),
+            ]),
+            Result = 2,
+        },
+        Solution = 0,
+    };
 }
