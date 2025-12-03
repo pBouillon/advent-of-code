4a20fe95eb9adedf296ecb4f8abd2659803f0ff7
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-12-03 22:09:24 +0100

    chore(2025): day 3 part 2 cleanup

--- src/2025/Day03/Solver.cs
+++ src/2025/Day03/Solver.cs
@@ -1,67 +1,46 @@
-using FluentAssertions;
-using System.Runtime.CompilerServices;
-using System.Text;
-using Xunit;
-
 namespace _2025.Day03;
 
-public static class Extentions
-{
-    public static int ToInt(this char @char) => @char - '0';
-}
-
 public class Solver()
-    : Solver<string[], long>("Day03/input.txt")
+    : Solver<int[][], long>("Day03/input.txt")
 {
-    public static (int Value, int Offset) MaxPossibleDigitIn(string bank)
+    public static long LongestPossibleNumberIn(int[] bank, int finalNumberLength)
     {
-        var bestDigit = (Value: bank[0].ToInt(), Offset: 0);
-
-        for (int i = 0; i < bank.Length; i++)
-        {
-            var current = (Value: bank[i].ToInt(), Offset: i);
-            if (current.Value > bestDigit.Value) bestDigit = current;
-        }
-
-        return bestDigit;
-    }
-
-    public static long LongestPossibleNumberIn(string bank, int finalNumberLength)
-    {
-        var buffer = new StringBuilder();
+        var result = 0L;
         var singleDigitsSkipped = 0;
 
         for (var currentPowerOfTen = finalNumberLength; currentPowerOfTen > 0; --currentPowerOfTen)
         {
             var numbersOfPowerOfTenTooBig = finalNumberLength - currentPowerOfTen;
-            var numbersOfPowerOfTenTooSmall = currentPowerOfTen - 1;
+            var reserved = currentPowerOfTen - 1;
 
-            var searchRange = bank[(numbersOfPowerOfTenTooBig + singleDigitsSkipped)..^numbersOfPowerOfTenTooSmall];
+            var window = bank[(numbersOfPowerOfTenTooBig + singleDigitsSkipped)..^reserved];
 
-            var nextBestDigit = MaxPossibleDigitIn(searchRange);
+            var (value, offset) = window
+                .Select((battery, index) => (Value: battery, Offset: index))
+                .MaxBy(battery => battery.Value);
 
-            buffer.Append(nextBestDigit.Value);
-            singleDigitsSkipped += nextBestDigit.Offset;
+            result += (long)(value * Math.Pow(10, currentPowerOfTen - 1));
+            singleDigitsSkipped += offset;
         }
 
-        return long.Parse(buffer.ToString());
+        return result;
     }
 
-    public override long PartOne(string[] strings)
+    public override long PartOne(int[][] strings)
         => strings
             .Select(bank => LongestPossibleNumberIn(bank, finalNumberLength: 2))
             .Sum();
 
-    public override long PartTwo(string[] strings)
+    public override long PartTwo(int[][] strings)
         => strings
             .Select(bank => LongestPossibleNumberIn(bank, finalNumberLength: 12))
             .Sum();
 
-    public override string[] ParseInput(IEnumerable<string> input)
-        => [.. input];
+    public override int[][] ParseInput(IEnumerable<string> input)
+        => [.. input.Select(bank => bank.Select(battery => battery - '0').ToArray())];
 }
 
-public class SolverTest : TestEngine<Solver, string[], long>
+public class SolverTest : TestEngine<Solver, int[][], long>
 {
     public override Puzzle PartOne => PuzzleBuilder
         .FromInput([
@@ -71,20 +50,20 @@ public class SolverTest : TestEngine<Solver, string[], long>
             "818181911112111",
         ])
         .ParsedAs([
-            "987654321111111",
-            "811111111111119",
-            "234234234234278",
-            "818181911112111",
+            [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+            [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+            [2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+            [8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
         ])
         .ExpectsResult(357)
         .WithTheActualSolutionBeing(17_613);
 
     public override Puzzle PartTwo => PuzzleBuilder
         .FromParsedInput([
-            "987654321111111",
-            "811111111111119",
-            "234234234234278",
-            "818181911112111",
+            [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1],
+            [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+            [2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8],
+            [8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1],
         ])
         .ExpectsResult(3_121_910_778_619)
         .WithTheActualSolutionBeing(175_304_218_462_560);
