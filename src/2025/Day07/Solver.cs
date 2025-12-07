2a79e12e88abdcb8f6b08ae731fff5c8f38e102c
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-12-07 13:31:22 +0100

    feat(2025): solves day 7 part 2

--- src/2025/Day07/Solver.cs
+++ src/2025/Day07/Solver.cs
@@ -1,9 +1,12 @@
+using AdventOfCode.Utils.Graph;
+using AdventOfCode.Utils.Matrix;
+
 namespace _2025.Day07;
 
 public class Solver()
-    : Solver<(int StartIndex, HashSet<int>[] SplittersIndexes)>("Day07/input.txt")
+    : Solver<Node<Coordinate>>("Day07/input.txt")
 {
-    public override (int StartIndex, HashSet<int>[] SplittersIndexes) ParseInput(IEnumerable<string> input)
+    public override Node<Coordinate> ParseInput(IEnumerable<string> input)
     {
         var diagram = input.ToArray();
 
@@ -16,116 +19,132 @@ public class Solver()
             .Where(indexes => indexes.Count > 0)
             .ToArray();
 
-        return (
-            StartIndex: diagram[0].IndexOf('S'),
-            SplittersIndexes: splittersIndexes
-        );
-    }
+        var root = new Node<Coordinate>(new Coordinate(
+            diagram[0].IndexOf('S'),
+            0));
 
-    public override string PartOne((int StartIndex, HashSet<int>[] SplittersIndexes) input)
-    {
-        var beamsIndexes = new HashSet<int> { input.StartIndex };
+        var leafs = new HashSet<Node<Coordinate>> { root };
 
-        var splitterHitCount = 0;
-
-        foreach (var splitters in input.SplittersIndexes)
+        for (var rowIndex = 0; rowIndex < splittersIndexes.Length; ++rowIndex)
         {
-            var splittersHit = splitters
-                .Where(beamsIndexes.Contains)
-                .ToArray();
+            var splitters = splittersIndexes[rowIndex];
 
-            splitterHitCount += splittersHit.Length;
+            var splittersHit = splitters
+                .Where(index => leafs.Any(node => node.Value.X == index))
+                .ToArray();
 
             foreach (var index in splittersHit)
             {
-                beamsIndexes.Remove(index);
+                var splittedBeam = leafs.First(node => node.Value.X == index);
 
-                beamsIndexes.Add(index - 1);
-                beamsIndexes.Add(index + 1);
+                leafs.Remove(splittedBeam);
+
+                var left = leafs.FirstOrDefault(node => node.Value.X == index - 1)
+                    ?? new Node<Coordinate>(
+                        splittedBeam.Value.MovedToThe(Direction.Left) with { Y = rowIndex + 1 }
+                    );
+
+                splittedBeam.AddUnidirectionalLinkTo(left);
+                leafs.Add(left);
+
+                var right = leafs.FirstOrDefault(node => node.Value.X == index + 1)
+                    ?? new Node<Coordinate>(
+                        splittedBeam.Value.MovedToThe(Direction.Right) with { Y = rowIndex + 1 }
+                    );
+
+                splittedBeam.AddUnidirectionalLinkTo(right);
+                leafs.Add(right);
             }
         }
 
-        return splitterHitCount.ToString();
+        return root;
     }
 
-    public override string PartTwo((int StartIndex, HashSet<int>[] SplittersIndexes) input)
-        => ComputeBeamPaths([input.StartIndex], input.SplittersIndexes)
-            .Length
-            .ToString();
-
-    private static int[][] ComputeBeamPaths(int[] beamPath, HashSet<int>[] SplittersIndexes)
+    public override string PartOne(Node<Coordinate> root)
     {
-        var beamIndex = beamPath[^1];
-        var splitters = SplittersIndexes[0];
+        var splittedBeamsCount = 0;
 
-        var isSplit = splitters.Contains(beamIndex);
+        var toExplore = new Stack<Node<Coordinate>>();
+        toExplore.Push(root);
 
-        int[] nextBeamIndexes = isSplit
-            ? [beamIndex - 1, beamIndex + 1]
-            : [beamIndex];
+        var explored = new HashSet<Coordinate>();
 
-        var nextBeamPaths = nextBeamIndexes
-            .Select(index => beamPath.Concat([index]).ToArray())
-            .ToArray();
+        while (toExplore.Count > 0)
+        {
+            var current = toExplore.Pop();
 
-        var isStillWithinDiagram = SplittersIndexes.Length > 1;
+            var isAlreadyExplored = explored.Contains(current.Value);
+            if (isAlreadyExplored) continue;
 
-        return isStillWithinDiagram
-            ? [.. nextBeamPaths.SelectMany(path => ComputeBeamPaths(path, SplittersIndexes[1..]))]
-            : nextBeamPaths;
+            explored.Add(current.Value);
+
+            var isSplittedBeam = !current.IsLeaf();
+            if (isSplittedBeam)
+            {
+                ++splittedBeamsCount;
+                current.ConnectsTo.ForEach(toExplore.Push);
+            }
+        }
+
+        return splittedBeamsCount.ToString();
+    }
+
+    public override string PartTwo(Node<Coordinate> root)
+    {
+        var cache = new Dictionary<Coordinate, long>();
+
+        long PossiblePathsToLeafsCount(Node<Coordinate> node)
+        {
+            var isCached = cache.ContainsKey(node.Value);
+            if (isCached)
+            {
+                return cache[node.Value];
+            }
+
+            var result = node.IsLeaf()
+                ? 1
+                : node.ConnectsTo.Sum(PossiblePathsToLeafsCount);
+
+            cache[node.Value] = result;
+
+            return result;
+        }
+
+        return PossiblePathsToLeafsCount(root).ToString();
     }
 }
 
 public class SolverTest
-    : TestEngine<Solver, (int StartIndex, HashSet<int>[] SplittersIndexes)>
+    : TestEngine<Solver, Node<Coordinate>>
 {
+    private readonly string[] _diagram = [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "...............",
+    ];
+
+    private Node<Coordinate> Root => new Solver().ParseInput(_diagram);
+
     public override Puzzle PartOne => PuzzleBuilder
-        .FromInput([
-            ".......S.......",
-            "...............",
-            ".......^.......",
-            "...............",
-            "......^.^......",
-            "...............",
-            ".....^.^.^.....",
-            "...............",
-            "....^.^...^....",
-            "...............",
-            "...^.^...^.^...",
-            "...............",
-            "..^...^.....^..",
-            "...............",
-            ".^.^.^.^.^...^.",
-            "...............",
-        ])
-        .ParsedAs((
-            StartIndex: 7,
-            SplittersIndexes: [
-                [7],
-                [6, 8],
-                [5, 7, 9],
-                [4, 6, 10],
-                [3, 5, 9, 11],
-                [2, 6, 12],
-                [1, 3, 5, 7, 9, 13],
-            ]
-        ))
+        .FromParsedInput(Root)
         .ExpectsResult("21")
         .WithTheActualSolutionBeing("1690");
 
     public override Puzzle PartTwo => PuzzleBuilder
-        .FromParsedInput((
-            StartIndex: 7,
-            SplittersIndexes: [
-                [7],
-                [6, 8],
-                [5, 7, 9],
-                [4, 6, 10],
-                [3, 5, 9, 11],
-                [2, 6, 12],
-                [1, 3, 5, 7, 9, 13],
-            ]
-        ))
+        .FromParsedInput(Root)
         .ExpectsResult("40")
-        .WithTheActualSolutionBeing("0");
+        .WithTheActualSolutionBeing("221371496188107");
 }
---
+++ src/AdventOfCode.Utils/Graph/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Utils.Graph;
+
+[DebuggerDisplay("{Value}, children: {ConnectsTo.Count}")]
+public sealed record Node<TValue>(TValue Value)
+{
+    public List<Node<TValue>> ConnectsTo { get; init; } = [];
+
+    public void AddBidirectionalLinkTo(Node<TValue> node)
+    {
+        AddUnidirectionalLinkTo(node);
+        node.AddUnidirectionalLinkTo(this);
+    }
+
+    public void AddUnidirectionalLinkTo(Node<TValue> node)
+        => ConnectsTo.Add(node);
+
+    public bool IsLeaf()
+        => ConnectsTo.Count == 0;
+
+    public HashSet<Node<TValue>> GetLeafs()
+        => IsLeaf()
+            ? [this]
+            : [.. ConnectsTo.SelectMany(node => node.GetLeafs())];
+}
