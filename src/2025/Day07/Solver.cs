15d78a0ca06ea5df4c792612a1bbc02afebe5290
pBouillon <pro.pierre.bouillon@protonmail.com>
2025-12-07 10:51:14 +0100

    feat(2025): add a very inefficient recursive solution for day 7 part 2

--- src/2025/Day07/Solver.cs
+++ src/2025/Day07/Solver.cs
@@ -1,9 +1,9 @@
 namespace _2025.Day07;
 
 public class Solver()
-    : Solver<(int StartIndex, int[][] SplittersIndexes)>("Day07/input.txt")
+    : Solver<(int StartIndex, HashSet<int>[] SplittersIndexes)>("Day07/input.txt")
 {
-    public override (int StartIndex, int[][] SplittersIndexes) ParseInput(IEnumerable<string> input)
+    public override (int StartIndex, HashSet<int>[] SplittersIndexes) ParseInput(IEnumerable<string> input)
     {
         var diagram = input.ToArray();
 
@@ -12,8 +12,8 @@ public class Solver()
                 .Select((symbol, index) => (symbol, index))
                 .Where(x => x.symbol == '^')
                 .Select(x => x.index)
-                .ToArray())
-            .Where(indexes => indexes.Length > 0)
+                .ToHashSet())
+            .Where(indexes => indexes.Count > 0)
             .ToArray();
 
         return (
@@ -22,7 +22,7 @@ public class Solver()
         );
     }
 
-    public override string PartOne((int StartIndex, int[][] SplittersIndexes) input)
+    public override string PartOne((int StartIndex, HashSet<int>[] SplittersIndexes) input)
     {
         var beamsIndexes = new HashSet<int> { input.StartIndex };
 
@@ -48,14 +48,36 @@ public class Solver()
         return splitterHitCount.ToString();
     }
 
-    public override string PartTwo((int StartIndex, int[][] SplittersIndexes) input)
+    public override string PartTwo((int StartIndex, HashSet<int>[] SplittersIndexes) input)
+        => ComputeBeamPaths([input.StartIndex], input.SplittersIndexes)
+            .Length
+            .ToString();
+
+    private static int[][] ComputeBeamPaths(int[] beamPath, HashSet<int>[] SplittersIndexes)
     {
-        throw new NotImplementedException();
+        var beamIndex = beamPath[^1];
+        var splitters = SplittersIndexes[0];
+
+        var isSplit = splitters.Contains(beamIndex);
+
+        int[] nextBeamIndexes = isSplit
+            ? [beamIndex - 1, beamIndex + 1]
+            : [beamIndex];
+
+        var nextBeamPaths = nextBeamIndexes
+            .Select(index => beamPath.Concat([index]).ToArray())
+            .ToArray();
+
+        var isStillWithinDiagram = SplittersIndexes.Length > 1;
+
+        return isStillWithinDiagram
+            ? [.. nextBeamPaths.SelectMany(path => ComputeBeamPaths(path, SplittersIndexes[1..]))]
+            : nextBeamPaths;
     }
 }
 
 public class SolverTest
-    : TestEngine<Solver, (int StartIndex, int[][] SplittersIndexes)>
+    : TestEngine<Solver, (int StartIndex, HashSet<int>[] SplittersIndexes)>
 {
     public override Puzzle PartOne => PuzzleBuilder
         .FromInput([
@@ -104,6 +126,6 @@ public class SolverTest
                 [1, 3, 5, 7, 9, 13],
             ]
         ))
-        .ExpectsResult("0")
+        .ExpectsResult("40")
         .WithTheActualSolutionBeing("0");
 }
